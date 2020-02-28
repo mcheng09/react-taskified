38ee01d7940a13a40f3e8454b6764482c58fcce7
Michael <mcheng.oc@gmail.com>
2020-02-28 13:38:16 -0800

    create(TasksContainer): create TasksContainer component

---
+++ src/containers/TasksContainer/TasksContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TasksContainer extends Component {
+  render () {
+    return (
+      <div className='tasks-container'>
+        <h1>Task Container Component</h1>
+      </div>
+    )
+  }
+}
+
+export default TasksContainer;
