126bce25bffc700e6a1fd19bfdb3abf1aaead8ba
Michael <mcheng.oc@gmail.com>
2020-02-28 14:48:05 -0800

    updates(TasksContainer): adds static data to state and imports UserCard

--- src/containers/TasksContainer/TasksContainer.js
+++ src/containers/TasksContainer/TasksContainer.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
 
+import UserCard from './../../components/UserCard/UserCard'
+
 class TasksContainer extends Component {
+  state = {
+    users: [
+      {
+        id: 0,
+        name: 'Mike',
+        tasks: [
+          'Build this App',
+          'Add styling to application',
+          'Build out functionalities'
+        ]
+      },
+      {
+        id: 1,
+        name: 'Some other random person',
+        tasks: [
+          'Buy some eggs',
+          'Do laundry',
+          'Take out the trash'
+        ]
+      }
+    ]
+  }
+
+
   render () {
+    const userCards = this.state.users.map(user => {
+      return <UserCard key={'user' + user.id} userData={user} />
+    })
+
     return (
       <div className='tasks-container'>
-        <h1>Task Container Component</h1>
+        { userCards }
       </div>
     )
   }
