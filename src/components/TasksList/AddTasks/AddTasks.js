4ffbcd5f5cf15a21f24759460b50d57325f7350c
Mike <mike@mikescodegarden.com>
2020-03-01 12:54:46 -0800

    update(AddTasks): updates markup in component

--- src/components/TasksList/AddTasks/AddTasks.js
+++ src/components/TasksList/AddTasks/AddTasks.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import classes from './AddTasks.module.scss'
+
 function AddTasks(props) {
   return (
-    <div>
-      <button onClick={() => props.addTask(props.userID)}>Click me!</button>
+    <div className={classes.AddTasks} onClick={() => props.addTask(props.userID)}>
+    <span>+</span>
+    <span>Add a task!</span>
     </div>
   )
 }
