0b636ba79e252baad5237c3b616f3c8a10832ec5
Michael <mcheng.oc@gmail.com>
2020-03-03 20:11:39 -0800

    updates(RemoveTasks): changes span to div tag

--- src/components/TasksList/RemoveTasks/RemoveTasks.js
+++ src/components/TasksList/RemoveTasks/RemoveTasks.js
@@ -4,7 +4,7 @@ import classes from './RemoveTasks.module.scss'
 
 function RemoveTasks(props) {
   return (
-    <span className={classes.RemoveTasks} onClick={() => props.removeTask(props.userID, props.taskIndex)}>X</span>
+    <div className={classes.RemoveTasks} onClick={() => props.removeTask(props.userID, props.taskIndex)}>X</div>
   )
 }
 
