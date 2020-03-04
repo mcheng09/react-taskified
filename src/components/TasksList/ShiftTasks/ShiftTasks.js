ada6cad32ef58c995465fc765548e5a5ccb4d2a7
Michael <mcheng.oc@gmail.com>
2020-03-03 20:11:16 -0800

    create(ShiftTasks): creates component and stylesheet

---
+++ src/components/TasksList/ShiftTasks/ShiftTasks.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import classes from './ShiftTasks.module.scss'
+
+function ShiftTasks () {
+  return (
+    <div className={classes.ShiftTasks}>
+      <div>{'<'}</div>
+      <div>{'>'}</div>
+    </div>
+  )
+}
+
+export default ShiftTasks;
